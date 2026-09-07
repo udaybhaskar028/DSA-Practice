ccb2a5a99bff052cd17a4556a9edf9fc9b51e403
udaybhaskar028 <pudaybhaskar2@gmail.com>
2026-09-08 02:59:21 +0530

    Time: 8 ms (50.35%), Space: 43.5 MB (98.42%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,27 +1,34 @@
-public class Solution {
+class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        if (s1.length() > s2.length())
-            return false;
-        int[] s1arr = new int[26];
-        int[] s2arr = new int[26];
-        for (int i = 0; i < s1.length(); i++) {
-            s1arr[s1.charAt(i) - 'a']++;
-            s2arr[s2.charAt(i) - 'a']++;
-        }
-        for (int i = 0; i < s2.length() - s1.length(); i++) {
-            if (matches(s1arr, s2arr))
-                return true;
-            s2arr[s2.charAt(i + s1.length()) - 'a']++;
-            s2arr[s2.charAt(i) - 'a']--;
-        }
-        return matches(s1arr, s2arr);
-    }
+        int n = s1.length(), m = s2.length();
+        if (n > m) return false;
 
-    public boolean matches(int[] s1arr, int[] s2arr) {
-        for (int i = 0; i < 26; i++) {
-            if (s1arr[i] != s2arr[i])
-                return false;
+        int[] need = new int[26];
+        int[] win  = new int[26];
+        for (int i = 0; i < n; i++) {
+            need[s1.charAt(i) - 'a']++;
+            win[s2.charAt(i) - 'a']++;
         }
-        return true;
+
+        int matches = 0;
+        for (int i = 0; i < 26; i++)
+            if (need[i] == win[i]) matches++;
+        if (matches == 26) return true;
+
+        for (int right = n; right < m; right++) {
+            int in  = s2.charAt(right) - 'a';
+            int out = s2.charAt(right - n) - 'a';
+
+            if (win[in] == need[in]) matches--;
+            win[in]++;
+            if (win[in] == need[in]) matches++;
+
+            if (win[out] == need[out]) matches--;
+            win[out]--;
+            if (win[out] == need[out]) matches++;
+
+            if (matches == 26) return true;
+        }
+        return false;
     }
 }
